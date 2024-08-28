e9eb3382ecbd241f95d837411488bf81aab2ce1d
LuisVynicius <luismevy@gmail.com>
2024-08-28 15:31:44 -0300

    Created UserRepository

---
+++ src/main/java/com/mevy/santander_dev_week_2023/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mevy.santander_dev_week_2023.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mevy.santander_dev_week_2023.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
